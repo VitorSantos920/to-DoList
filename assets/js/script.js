1deaa6364f239af3e3fd14856c70bfabf830e410
Vitor Santos <pireshugo737@gmail.com>
2022-05-18 22:24:41 -0300

    Início da estilização: Divs e borders / Correções de Layout e códigos JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,8 @@
-function clickButton() {
-    let campo = document.getElementById("inpText"); 
-    alert(campo.value); // .value -> Valor do campo
-}
-
 function addItem(){
     let campo = document.getElementById("inpText"); // Resgatando o elemento HTML pelo ID
 
     // Verificação se o campo está vazio
-    if(campo.value.trim()){
+    if(campo.value.trim()){ // .trim() para remover espaços em branco
         let elemento = document.createElement("li"); // Criando item HTML 'li' | Armazenando em -> 'item'
     
         elemento.innerHTML = campo.value; // 'Conteúdo do elemento criado (li) terá como conteúdo o valor do input'
