a73785dfda659d52309e865f35a685c2fd435531
Vitor Santos <pireshugo737@gmail.com>
2022-05-24 00:03:16 -0300

    Correções / Ajustes / Botão Remover em Andamento

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@ var tarefas = [];
 function mostrarFuncao(){
     let lista = document.getElementById("list_itens"); // resgatando a lista
     lista.innerHTML = "";
-    
+
     for (const item of tarefas) {
         let template = document.getElementById("template"); // Template 
         let itemNovo = template.content.cloneNode(true); // Clone
@@ -10,20 +10,8 @@ function mostrarFuncao(){
         let li = itemNovo.querySelector("li"); // Li
         li.append(item); // Adicionar o item da vez no li
         lista.append(li); // Adicionar o li da vez na lista
-    // Verificação se o campo está vazio
     }
-    if(campo.value.trim()){ // .trim() para remover espaços em branco
-        let template = document.getElementById("template").content.cloneNode(true);
-        let input = document.getElementById("inpText");
-        let li = template.querySelector("li");
-        
-        li.append(input.value);
-        document.getElementById("list_itens").append(li);
-    }
-    else{
-        alert("Insira um texto")
-    }
-
+}
 function adicionarTarefas() {
     let inputTarefa = document.getElementById("inpText");
     tarefas.push(inputTarefa.value); // Insere na array o valor do input
@@ -33,9 +21,7 @@ function adicionarTarefas() {
 }
 
 function removerItem() {
-    debugger;
     let li = document.querySelector("li");
 
     li.remove(li)
-    }
 }
