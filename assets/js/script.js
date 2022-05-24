ccd69dcece430aa7bc38b2bf034efa0ae7f9a343
Vitor Santos <pireshugo737@gmail.com>
2022-05-23 22:47:58 -0300

    Tarefas em Array / Inserção na Tela com Forof

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,28 @@
-function addItem(){
-    let campo = document.getElementById("inpText"); // Resgatando o elemento HTML pelo ID
+var tarefas = [];
+function mostrarFuncao(){
+    let lista = document.getElementById("list_itens"); // resgatando a lista
+    lista.innerHTML = "";
 
-    // Verificação se o campo está vazio
-    if(campo.value.trim()){ // .trim() para remover espaços em branco
-        let template = document.getElementById("template").content.cloneNode(true);
-        let input = document.getElementById("inpText");
-        let li = template.querySelector("li");
-        
-        li.append(input.value);
-        document.getElementById("list_itens").append(li);
+    for (const item of tarefas) {
+        let template = document.getElementById("template"); // Template 
+        let itemNovo = template.content.cloneNode(true); // Clone
 
-    } else{
-        alert("Insira um texto")
+        let li = itemNovo.querySelector("li"); // Li
+        li.append(item); // Adicionar o item da vez no li
+        lista.append(li); // Adicionar o li da vez na lista
     }
-    
+}
+function adicionarTarefas() {
+    let inputTarefa = document.getElementById("inpText");
+    tarefas.push(inputTarefa.value); // Insere na array o valor do input
+    mostrarFuncao(); // invoca a função
+
+    inputTarefa.value = "";
+}
+
+function removerItem() {
+    debugger;
+    let li = document.querySelector("li");
+
+    li.remove(li)
 }
